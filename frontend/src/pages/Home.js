fe9a29920dbbf0d1d743f2326e1f1bf75e9e652b
Yu <zyken9054@gmail.com>
2022-09-17 17:26:35 -0400

    frontend honepage component and details component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+
+// components
+import WorkoutDetails from '../components/WorkoutDetails'
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+
+    // only fire once when page first renders
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+
+        fetchWorkouts();
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
         </div>
     )
 }
